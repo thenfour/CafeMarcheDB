751db4e0ed5e6bc4141266335b751dd93cfe0dd9
carl <carlco@gmail.com>
2023-06-23 16:18:21 +0200

    tutorial changes done

---
+++ 03-blitz/cmdb2/src/questions/schemas.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const CreateQuestionSchema = z.object({
+  text: z.string(),
+  choices: z.array(z.object({text: z.string()})),
+  // template: __fieldName__: z.__zodType__(),
+});
+export const UpdateQuestionSchema = z.object({
+  id: z.number(),
+  //text: z.string(),
+  // template: __fieldName__: z.__zodType__(),
+});
+
+export const DeleteQuestionSchema = z.object({
+  id: z.number(),
+});
