79df4636f6b6c656b4ba591068738f99ce13bc95
carl <carlco@gmail.com>
2023-07-12 21:36:58 +0200

    markdown editing is getting better

--- 03-blitz/cmdb2/src/core/components/RichTextEditor.tsx
+++ 03-blitz/cmdb2/src/core/components/RichTextEditor.tsx
@@ -3,151 +3,103 @@
 // ok we're close. now there's a sequence of events creating a flicker
 // debounce starts, we use an internal value.
 // debounce stops, now we use the passed in value (which is not correct)
+// so there needs to be many intermediate steps, between both the text editor and the caller.
+// better to just make it uncontrolled, so what the user sees is ALWAYS correct.
 
 import React, { Suspense } from "react";
-import { Box, CircularProgress, Tab, Tabs, TextField, Typography } from "@mui/material";
-//import { MuiMarkdown } from 'mui-markdown';
+import { Box, Button, ButtonGroup, CircularProgress, IconButton, LinearProgress, Tab, Tabs, TextField, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material";
 import MarkdownIt from 'markdown-it';
 import useDebounce from "shared/useDebounce";
-
-
-// interface TabPanelProps {
-//     children?: React.ReactNode;
-//     index: number;
-//     value: number;
-// }
-
-// function CustomTabPanel(props: TabPanelProps) {
-//     const { children, value, index, ...other } = props;
-
-//     return (
-//         <div
-//             role="tabpanel"
-//             hidden={value !== index}
-//             id={`simple-tabpanel-${index}`}
-//             aria-labelledby={`simple-tab-${index}`}
-//             {...other}
-//         >
-//             {value === index && (
-//                 <Box >
-//                     {children}
-//                 </Box>
-//             )}
-//         </div>
-//     );
-// }
-
-
-// function a11yProps(index: number) {
-//     return {
-//         id: `simple-tab-${index}`,
-//         'aria-controls': `simple-tabpanel-${index}`,
-//     };
-// }
-
-// interface DebounceArgs {
-//     fn: (any) => any,
-//     milliseconds: number,
-// };
-
-// const Debounce = (args: DebounceArgs) => {
-//     let timeoutId;
-//     return function (...innerArgs) {
-//         clearTimeout(timeoutId);
-//         timeoutId = setTimeout(() => {
-//             args.fn.apply(this, innerArgs);
-//         }, args.milliseconds);
-//     };
-// }
+import {
+    Add as AddIcon,
+    Close as CancelIcon,
+    DeleteOutlined as DeleteIcon,
+    Edit as EditIcon,
+    Save as SaveIcon
+} from '@mui/icons-material';
 
 interface RichTextEditorProps {
-    value: string, // value which may be stored in the database.
+    initialValue: string, // value which may be coming from the database.
     onValueChanged: (val: string) => void, // caller can save the changed value to a db here.
-    saved: boolean, // indicate whether the value is saved or not to the db.
+    isSaving: boolean, // show the value as saving in progress
     debounceMilliseconds: number,
 }
 
+// we will control the value, but callers must treat as uncontrolled.
+interface RichTextEditorValueState {
+    markdown: string,
+    html: string,
+}
+
+function MarkdownToHTML(md: any, value: string): string {
+    return md.render(value);
+}
+
+
 export default function RichTextEditor(props: RichTextEditorProps) {
-    const [nonDebouncedValue, setNonDebouncedValue] = React.useState<string>("");
-    //const [tab, setTab] = React.useState<Number>(0);
-    // const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    //     setTab(newValue);
-    // };
+    const [md, setMd] = React.useState<any>(null);
+    const [valueState, setValueState] = React.useState<RichTextEditorValueState>({
+        markdown: "",
+        html: "",
+    });
 
     const [isDebouncing, setIsDebouncing] = React.useState<boolean>(false);
-    const debouncedValue = useDebounce<string>(nonDebouncedValue || "", props.debounceMilliseconds); // 
+    const debouncedMarkdown = useDebounce<string>(valueState.markdown, props.debounceMilliseconds); // 
+
+    React.useEffect(() => {
+        const md2 = new MarkdownIt;
+        setMd(md2);
+        setValueState({
+            markdown: props.initialValue,
+            html: md2.render(props.initialValue),
+        });
+    }, []);
 
     // when debounced value changes, tell caller to commit.
     React.useEffect(() => {
         setIsDebouncing(false);
-        if (props.onValueChanged) {
-            //console.log(`debounce complete; val=${debouncedValue}. calling caller.onValueChanged`);
-            props.onValueChanged(debouncedValue);
-        }
-        //console.log(`value committed: ${debouncedValue}`);
-    }, [debouncedValue]);
+        if (props.onValueChanged) props.onValueChanged(debouncedMarkdown);
+    }, [debouncedMarkdown]);
 
     // raw onchange; set internal state for temp use while debouncing
     const onChange = (e) => {
         const newval = e.target.value;
         setIsDebouncing(true);
-        setNonDebouncedValue(newval);
-        //console.log(`raw value change in text editor. isDebouncing=true, nondebouncedvalue=${newval}`);
+        console.assert(!!md);
+        setValueState({
+            markdown: newval,
+            html: md.render(newval),
+        });
     };
 
-    const value = (isDebouncing ? nonDebouncedValue : props.value) || "";
-
-    const [html, setHtml] = React.useState<string>();
-
-    // make html track value
-    const [md] = React.useState(new MarkdownIt());
-    React.useEffect(() => {
-        setHtml(md.render(value));
-    }, [value]);
-
-    const isDirty = isDebouncing || !props.saved;
-
-    //console.log(`text editor rendering with value:(${value})`);
+    const [showingEditor, setShowingEditor] = React.useState<boolean>(false);
 
     return (
-        <>
-            {/* <Suspense fallback="loading..."> */}
-            <Box sx={{ height: 40 }}>
-                {isDirty ? (<CircularProgress size={24} />) : (<>up to date.</>)}
-            </Box>
-            <Box>
-                <TextField sx={{ width: "100%" }} multiline={true} value={value} onChange={onChange} variant="filled"></TextField>
-            </Box>
-            <Box sx={{ border: 3 }}>
-                <div dangerouslySetInnerHTML={{ __html: html }}></div>
+        <Box sx={{ border: 2 }}>
+
+            <ButtonGroup variant="outlined" size="small" aria-label="outlined primary button group">
+                <Button startIcon={<EditIcon />} onClick={() => { setShowingEditor(!showingEditor) }} >Edit</Button>
+                <Button startIcon={<DeleteIcon />}>Delete</Button>
+            </ButtonGroup>
+            {props.isSaving ? (<LinearProgress color="info" sx={{ width: 25 }} />) : (
+                isDebouncing ? (<LinearProgress color="warning" sx={{ width: 25 }} />) : (
+                    <LinearProgress variant="determinate" value={100} color="success" sx={{ width: 25 }} />
+                )
+            )}
+
+            {showingEditor && (<>
+                <Box sx={{ p: 0 }}>
+                    <TextField sx={{ width: "100%" }} multiline={true} value={valueState.markdown} onChange={onChange} variant="filled"></TextField>
+                </Box>
+            </>
+            )}
+            <Box sx={{ p: 0 }}>
+                <div dangerouslySetInnerHTML={{ __html: valueState.html }}></div>
+
+
             </Box >
-            {/* </Suspense> */}
 
-
-            {/* <Tabs value={tab} onChange={handleChange} aria-label="basic tabs example">
-                <Tab label="Edit" {...a11yProps(0)} />
-                <Tab label="Preview" {...a11yProps(1)} />
-                <Tab label="Both" {...a11yProps(2)} />
-            </Tabs>
-            <CustomTabPanel index={0} value={tab}>
-                <Box>
-                    <TextField sx={{ width: "100%" }} multiline={true} value={value} onChange={e => setValue(e.target.value)}></TextField>
-                </Box>
-            </CustomTabPanel>
-            <CustomTabPanel index={1} value={tab}>
-                <Box sx={{ border: 1 }}>
-                    <MuiMarkdown>{value}</MuiMarkdown>
-                </Box>
-            </CustomTabPanel>
-            <CustomTabPanel index={2} value={tab}>
-                <Box>
-                    <TextField sx={{ width: "100%" }} multiline={true} value={value} onChange={e => setValue(e.target.value)}></TextField>
-                </Box>
-                <Box sx={{ border: 1 }}>
-                    <MuiMarkdown>{value}</MuiMarkdown>
-                </Box>
-            </CustomTabPanel> */}
-        </>
+        </Box>
     );
 }
 
--- 03-blitz/cmdb2/src/pages/info.tsx
+++ 03-blitz/cmdb2/src/pages/info.tsx
@@ -1,118 +1,37 @@
 'use client'
-//import { BlitzPage, dynamic } from "blitz"
+
 import { BlitzPage } from "@blitzjs/next";
 import { useMutation, useQuery } from "@blitzjs/rpc";
-import getSetting from "src/auth/queries/getSetting";
-import updateSettingMutation from "src/auth/mutations/updateSetting";
-import RichTextEditor from "src/core/components/RichTextEditor";
-import DashboardLayout from "src/core/layouts/DashboardLayout";
 import React, { Suspense } from "react";
+import updateSettingMutation from "src/auth/mutations/updateSetting";
+import getSetting from "src/auth/queries/getSetting";
+import RichTextEditor from "src/core/components/RichTextEditor";
 import { SnackbarContext } from "src/core/components/SnackbarContext";
+import DashboardLayout from "src/core/layouts/DashboardLayout";
 
-import NoSSR from 'react-no-ssr';
-//import RichTextEditorDraft from "src/core/components/RichTextEditorDraft";
-
-//import ReactDOM from 'react-dom';
-//import { Editor, EditorState } from 'draft-js';
-//import 'draft-js/dist/Draft.css';
-//import { Editor } from "react-draft-wysiwyg";
-//import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-//import { EditorState } from 'draft-js';
-//import 'react-quill/dist/quill.snow.css'
-//import SunEditor from 'suneditor-react';
-//import 'suneditor/dist/css/suneditor.min.css'; // Import Sun Editor's CSS File
-
-//import StarterKit from '@tiptap/starter-kit'
-
-//import { useEditor, EditorContent } from '@tiptap/react'
-
-// const Tiptap = () => {
-//     const editor = useEditor({
-//         extensions: [
-//             //StarterKit,
-//         ],
-//         content: '<p>Hello World! 🌎️</p>',
-//     })
-
-//     return (
-//         <EditorContent editor={editor} />
-//     )
-// }
-
-
-
-
-// import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-
-// const TextEditor = () => {
-//     const Editor = dynamic(
-//         () => import('react-draft-wysiwyg').then((mod) => mod.Editor),
-//         { ssr: false }
-//     )
-//     return (
-//         <>
-//             <div className="container my-5">
-//                 <Editor
-//                 />
-//             </div>
-//         </>
-//     )
-// }
-
-
-// const QuillNoSSRWrapper = dynamic(import('react-quill'), {
-//     ssr: false,
-//     loading: () => <p>Loading ...</p>,
-// })
-
-
-// const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
-//const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
-
-interface MyProps {
-
-};
-
-//props: React.PropsWithChildren<MyProps>
-const MyContent = (props: React.PropsWithChildren<MyProps>) => {
-    // we want to use the db value, but also want to show the user's typed value while the db saves.
-    let [value, { refetch }] = useQuery(getSetting, "info_text");
-    const [optimisticValue, setOptimisticValue] = React.useState<string>(value || "");
+const MyContent = () => {
+    let [initialValue, { refetch }] = useQuery(getSetting, "info_text");
     const [updateSetting] = useMutation(updateSettingMutation);
-    const [saved, setSaved] = React.useState(true);
+    const [isSaving, setIsSaving] = React.useState(true);
     const { showMessage: showSnackbar } = React.useContext(SnackbarContext);
 
     const onValueChanged = (newValue: string) => {
-        console.log(`-> caller onValueChanged('${newValue}'). setting saved=false and switching to optimistic`);
-        setOptimisticValue(newValue); // while we wait for the save to take place, keep the value optimistically.
-        setSaved(false);
+        setIsSaving(true);
         updateSetting({ name: "info_text", value: newValue }).then(x => {
             showSnackbar({ severity: "success", children: `should be saved...` });
-            setSaved(true);
-            console.log(`-> mutation complete; setting saved=true and switching to db value`);
+            setIsSaving(false);
             refetch();
         }).catch(e => {
             showSnackbar({ severity: "error", children: `error when updating setting` });
         });
     };
 
-    //const publicValue = saved ? value : optimisticValue;
-    const publicValue = optimisticValue;
-
-    console.log(`caller rendering. publicValue:${publicValue}, dbvalue:${value}, saved:${saved}`);
-
-    return <RichTextEditor value={publicValue || ""} saved={saved} onValueChanged={onValueChanged} debounceMilliseconds={500}></RichTextEditor>;
+    return <RichTextEditor initialValue={initialValue || ""} isSaving={isSaving} onValueChanged={onValueChanged} debounceMilliseconds={500}></RichTextEditor>;
 };
 
 
 
 const Info9Page: BlitzPage = () => {
-
-    // return (
-    //     <NoSSR fallback="Loading...">
-    //         <MyContent>info</MyContent>
-    //     </NoSSR>
-    // );
 
     return (
         <DashboardLayout title="Info">
@@ -121,18 +40,6 @@ const Info9Page: BlitzPage = () => {
             </Suspense>
         </DashboardLayout>
     );
-
-
-
-    // return (
-    //     // <NoSSR fallback="Loading...">
-    //     //<DashboardLayout title="Info">
-    //     {/* <Suspense fallback="Loading..."> */ }
-    // {/* <RichTextEditor value={value} saved={saved} onValueChanged={null}></RichTextEditor> */ }
-    // {/* </Suspense> */ }
-    //     //</DashboardLayout>
-    //     // </NoSSR>
-    // )
 }
 
 export default Info9Page;
