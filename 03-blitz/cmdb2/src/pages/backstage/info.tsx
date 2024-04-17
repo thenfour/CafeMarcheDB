c41d492725cceb9c63d581c2304e0fc37b53f3f5
carl <carlco@gmail.com>
2024-04-17 20:30:06 +0200

    info page tweak

--- 03-blitz/cmdb2/src/pages/backstage/info.tsx
+++ 03-blitz/cmdb2/src/pages/backstage/info.tsx
@@ -1,25 +1,20 @@
 'use client'
 
 import { BlitzPage } from "@blitzjs/next";
-import React, { Suspense } from "react";
+import { Suspense } from "react";
 import { Permission } from "shared/permissions";
 import { useAuthorizationOrThrow } from "src/auth/hooks/useAuthorization";
-import updateSettingMutation from "src/auth/mutations/updateSetting";
-import getSetting from "src/auth/queries/getSetting";
-import { MarkdownControl } from "src/core/components/RichTextEditor";
 import { SettingMarkdown } from "src/core/components/SettingMarkdown";
-import { SnackbarContext } from "src/core/components/SnackbarContext";
 import DashboardLayout from "src/core/layouts/DashboardLayout";
 
 const MyContent = () => {
 
-    // todo: what is this page
     useAuthorizationOrThrow("info page", Permission.basic_trust);
 
+    // this page is for like, general Café Marché info. but ideally we have a full wiki. we're not far off but ya....
 
     return <div>
         <SettingMarkdown setting="info_text" />
-        <input />
     </div>;
 };
 
