ca9ad935046174490df349fef09855b16f53d117
carl <carlco@gmail.com>
2024-04-22 02:18:54 +0200

    #85 adding warning for new members who need better access.

--- src/pages/backstage/index.tsx
+++ src/pages/backstage/index.tsx
@@ -148,9 +148,17 @@ const DashboardInner = () => {
 const DynamicContent = () => {
   const [currentUser] = useCurrentUser();
   let noInstrumentsWarning = (currentUser?.instruments?.length || 0) < 1;
+  let limitedAccountWarning = !!(currentUser?.role?.isRoleForNewUsers);
 
   return (<Suspense>
     <SettingMarkdown setting="BackstageFrontpageMarkdown" />
+
+    {limitedAccountWarning && <CMSinglePageSurfaceCard className="noInstrumentsWarning">
+      <div>{gIconMap.ErrorOutline()}</div>
+      <div>By default your account has limited access. Please contact an admin to gain access to all site features.
+      </div>
+    </CMSinglePageSurfaceCard>}
+
     {noInstrumentsWarning && <CMSinglePageSurfaceCard className="noInstrumentsWarning">
       <div>{gIconMap.ErrorOutline()}</div>
       <div>You have no instruments assigned; please go to
@@ -158,6 +166,8 @@ const DynamicContent = () => {
         and specify your instruments.
       </div>
     </CMSinglePageSurfaceCard>}
+
+
     <DashboardInner />
   </Suspense>
   )
