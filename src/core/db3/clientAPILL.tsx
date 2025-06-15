d624ba632215a51f18de80014825e5ac1f681a96
tenfour <carlco@gmail.com>
2025-06-15 17:26:39 +0200

    #502 fixed tempo display

--- src/core/db3/clientAPILL.tsx
+++ src/core/db3/clientAPILL.tsx
@@ -72,27 +72,29 @@ export function getURLClass(url: string, baseDomain: string = window.location.ho
     }
 }
 
-// export function getFormattedBPM(song: Prisma.SongGetPayload<{ select: { startBPM: true, endBPM: true } }>) {
-//     if (!song.startBPM) {
-//         if (!song.endBPM) {
-//             return "";// neither specified
-//         }
-//         return `⇢${song.endBPM}`; // only end bpm
-//     }
-//     if (!song.endBPM) {
-//         return `${song.startBPM}⇢`; // only start bpm
-//     }
-//     if ((song.startBPM | 0) === (song.endBPM | 0)) {
-//         return `${song.startBPM}`; // both BPMs the same: just show 1.
-//     }
-//     return `${song.startBPM}⇢${song.endBPM}`;
-// }
-
-
-export function getFormattedBPM(song: Prisma.SongGetPayload<{ select: { startBPM: true } }>) {
+// i don't know why i wrote this function twice; if issues happen because of this one,
+// explain it.
+export function getFormattedBPM(song: Prisma.SongGetPayload<{ select: { startBPM: true, endBPM: true } }>) {
     if (!song.startBPM) {
-        return "";
+        if (!song.endBPM) {
+            return "";// neither specified
+        }
+        return `⇢${song.endBPM}`; // only end bpm
     }
-    return `${song.startBPM}`;
+    if (!song.endBPM) {
+        return `${song.startBPM}⇢`; // only start bpm
+    }
+    if ((song.startBPM | 0) === (song.endBPM | 0)) {
+        return `${song.startBPM}`; // both BPMs the same: just show 1.
+    }
+    return `${song.startBPM}⇢${song.endBPM}`;
 }
+
+
+// export function getFormattedBPM(song: Prisma.SongGetPayload<{ select: { startBPM: true } }>) {
+//     if (!song.startBPM) {
+//         return "";
+//     }
+//     return `${song.startBPM}`;
+// }
 
