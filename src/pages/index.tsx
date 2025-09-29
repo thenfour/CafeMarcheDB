8f0b84b834b643455a308cbe1d29e84d4dbc8c26
tenfour <carlco@gmail.com>
2025-09-29 18:07:43 +0200

    build fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,10 @@
 import { BlitzPage } from "@blitzjs/next";
 import { GetServerSideProps } from "next";
-import * as db3 from "src/core/db3/db3";
+import { HostingMode, getHostingMode } from "shared/brandConfig";
 import { CMPublicIndex } from "../core/components/frontpage/CMFrontpage";
 import { GenericPublicIndex } from "../core/components/frontpage/GenericFrontpage";
-import { HostingMode, getHostingMode } from "shared/brandConfig";
-
-// type PublicIndexProps = {
-//     hostingMode: db3.HostingMode;
-// };
 
 const PublicIndex: BlitzPage<{}> = (props) => {
-    // hostingMode is available for future conditional rendering if desired
-    // const mode = props.hostingMode as db3.HostingMode | null;
     switch (getHostingMode()) {
         case HostingMode.CafeMarche:
             return <CMPublicIndex />;
